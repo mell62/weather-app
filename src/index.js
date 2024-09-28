fdb3bcc69fa956c2bb864c602b725fca7cc6606b
Mridhul <mridhul67@gmail.com>
2024-09-29 00:19:21 +0530

    Refactor code

--- src/index.js
+++ src/index.js
@@ -11,15 +11,11 @@ const locationForm = document.querySelector(".location-form");
 const celsiusBtn = document.querySelector(".celsius-btn");
 const fahrenheitBtn = document.querySelector(".fahrenheit-btn");
 
-const populateData = async function dataPopulator() {
-  fetchWeatherWhileLoading();
-};
-
 locationForm.addEventListener("submit", (event) => event.preventDefault());
-locationForm.addEventListener("submit", populateData);
+locationForm.addEventListener("submit", fetchWeatherWhileLoading);
 
 celsiusBtn.addEventListener("click", activateCelsius);
-celsiusBtn.addEventListener("click", populateData);
+celsiusBtn.addEventListener("click", fetchWeatherWhileLoading);
 
 fahrenheitBtn.addEventListener("click", activateFahrenheit);
-fahrenheitBtn.addEventListener("click", populateData);
+fahrenheitBtn.addEventListener("click", fetchWeatherWhileLoading);
