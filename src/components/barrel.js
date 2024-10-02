127e7841760d8a1dfe01e08f6efa13bd445be12a
Mridhul <mridhul67@gmail.com>
2024-10-02 23:21:52 +0530

    Write function to dynamically change hourly icons

--- src/components/barrel.js
+++ src/components/barrel.js
@@ -1,7 +1,8 @@
 export { searchLocation, errorHandler, removeError } from "./app";
 export { currentWeather } from "./current-interface";
-export { getCurrentIcon } from "./weather-icon-interface";
+export { getCurrentIcon } from "./current-icon-interface";
 export { hourlyWeather, slicedHour } from "./hourly-interface";
+export { getHourlyIcon } from "./hourly-icon-interface";
 export { todaysWeather } from "./today-interface";
 export { getMoonphaseIcon } from "./moonphase-icon-interface";
 export { dailyWeather } from "./daily-interface";
--- src/components/hourly-interface.js
+++ src/components/hourly-interface.js
@@ -1,5 +1,5 @@
 export { hourlyWeather, slicedHour };
-import { searchLocation } from "./barrel";
+import { searchLocation, getHourlyIcon } from "./barrel";
 
 const hourEles = document.querySelectorAll(".hourly-item");
 
@@ -8,15 +8,25 @@ const slicedHour = function sliceHour(hour) {
   return slicedHourTime;
 };
 
+const hourlyIcon = function returnHourlyIcon(data) {
+  console.log(data);
+
+  const icon = data.icon;
+  return getHourlyIcon(icon);
+};
+
 const hourlyWeather = async function populateHourlyWeather() {
   const weatherData = await searchLocation();
   const hourlyData = weatherData.hourlyData;
+  console.log(hourlyData);
 
   hourEles.forEach((hourEle, index) => {
     const timeEle = hourEle.querySelector(".hour-time");
     const conditionEle = hourEle.querySelector(".hour-condition");
     const temperatureEle = hourEle.querySelector(".hour-temperature");
+    const iconEle = hourEle.querySelector(".hourly-icon-container");
     timeEle.textContent = slicedHour(hourlyData[index].datetime);
+    iconEle.innerHTML = hourlyIcon(hourlyData[index]);
     conditionEle.textContent = hourlyData[index].conditions;
     temperatureEle.textContent = `${hourlyData[index].temp}°`;
   });
