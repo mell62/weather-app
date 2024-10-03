c1a36abf8c8ccb579397491af39266dd5f55bb09
Mridhul <mridhul67@gmail.com>
2024-10-04 01:24:46 +0530

    Add style and content to loading interface

--- src/components/loading-component.js
+++ src/components/loading-component.js
@@ -12,6 +12,20 @@ import {
 const loadingEle = document.querySelector(".loading-interface");
 
 const enableLoading = function enableLoadingFunction() {
+  const message1 = "Open sesa...memory palace!";
+  const message2 = "Investigating rainy footprints...";
+  const message3 =
+    "When you have eliminated the impossible, whatever remains, however improbable, must be the weather.";
+  const message4 = "Noting down suspicious hints of ice...";
+  const message5 = "It's a beautiful day! According to my deduction.";
+  const loadingMessagesArray = [
+    message1,
+    message2,
+    message3,
+    message4,
+    message5,
+  ];
+  loadingEle.textContent = loadingMessagesArray[Math.floor(Math.random() * 5)];
   loadingEle.style.display = "block";
 };
 
